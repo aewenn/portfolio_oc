51a7e907c3e3cb94076d0d7afe384288357cb2fa
Charlène Baldi <charlene.baldi6@gmail.com>
2024-07-06 12:29:53 +0200

    Affichage dynamique des projets

--- script.js
+++ script.js
@@ -51,3 +51,62 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', handleScroll);
     handleScroll();
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('projects.json') // Requête pour récupérer le fichier 'projects.json'
+        .then(response => response.json())
+        .then(data => {
+            const projectsContainer = document.querySelector('.projects-content'); // Sélectionne le conteneur des projets
+            projectsContainer.innerHTML = ''; // Vide le contenu actuel de l'élément
+
+            // Associe les compétences aux icônes correspondantes
+            const skillIconMap = {
+                "HTML": "bx bxl-html5",
+                "CSS": "bx bxl-css3",
+                "JavaScript": "bx bxl-javascript",
+                "Sass": "bx bxl-sass",
+                "React": "bx bxl-react",
+                "Redux": "bx bxl-redux",
+                "Git": "bx bxl-git",
+                "GitHub": "bx bxl-github",
+                "Figma": "bx bxl-figma",
+                "SEO": "bx bxs-chart",
+                "WAVE": "bx bxs-wrench",
+                "Google Rich Snippets": "bx bxs-file",
+                "Gestion de projet": "bx bxs-briefcase",
+                "Veille technique": "bx bxs-bulb",
+                "Notion": "bx bxs-file",
+                "Trello": "bx bxl-trello",
+                "Swagger": "bx bxs-network-chart",
+            };
+
+            data.projects.forEach(project => { // On parcourt chaque projet dans les données JSON
+                const projectItem = document.createElement('div');
+                projectItem.className = 'project-item';
+
+                // On définit le contenu HTML de chaque projet
+                projectItem.innerHTML = `
+                    <img src="${project.image}" alt="${project.alt}" title="Image du projet ${project.title}">
+                    <h3>${project.title}</h3>
+                    <div class="project-skills">
+                        ${project.skills.map(skill => {
+                            const iconClass = skillIconMap[skill] || 'bx bxs-help-circle';
+                            return `<span title="${skill}"><i class='${iconClass}'></i></span>`;
+                        }).join('')}
+                    </div>
+                    <p>${project.description.part1}</p>
+                    <p>${project.description.part2}</p>
+                    <div class="project-links">
+                        ${project.links.github ? `<a href="${project.links.github}" target="_blank" title="Lien vers le code du projet ${project.title}" class="social-icon"><span><i class='bx bxl-github'></i></span></a>` : ''}
+                        ${project.links.live ? `<a href="${project.links.live}" target="_blank" title="Lien vers le site du projet ${project.title}" class="social-icon"><span><i class='bx bx-link-external'></i></span></a>` : ''}
+                        ${project.links.presentation ? `<a href="${project.links.presentation}" target="_blank" title="Lien vers la présentation du projet ${project.title}" class="social-icon" target="_blank"><span><i class='bx bx-slideshow'></i></span></a>` : ''}
+                        ${project.links.report ? `<a href="${project.links.report}" target="_blank" title="Lien vers le rapport d'optimisation pour le projet ${project.title}" class="social-icon" target="_blank"><span><i class='bx bxs-file-pdf'></i></span></a>` : ''}
+                    </div>
+                `;
+                // On ajoute le projet au conteneur de projets
+                projectsContainer.appendChild(projectItem);
+            });
+        })
+        .catch(error => console.error('Error loading projects:', error));
+});
